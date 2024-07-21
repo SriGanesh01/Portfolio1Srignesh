94668b3e38686c9ebe72628d080e3a7edb0fa9c1
SriGanesh01 <psriganesh2005@gmail.com>
2024-07-21 11:07:02 +0530

    initsetup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+// import './App.css'
+
+import Home from './pages/Home'
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+            <ToastContainer />
+        </>
+    )
+}
+
+export default App
