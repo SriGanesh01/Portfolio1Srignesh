cca137bcec362b4ded0874a772e4b98f75b2bc25
SriGanesh01 <psriganesh2005@gmail.com>
2025-07-04 10:36:33 +0530

    BugTest

--- src/components/Home/HeroHome.jsx
+++ src/components/Home/HeroHome.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import Header from "../Header";
 
